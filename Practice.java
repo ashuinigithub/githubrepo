1f086f6b1ec22e47a064a0519bb5c76d6fd5b336
Ashvini <gopchudeashvini@gmail.com>
2022-09-01 13:21:40 +0530

    This is my first commit

---
+++ Practice.java
@@ -0,0 +1,19 @@
+package gitHub;
+
+public class Practice {
+	
+	public static void main(String[] args) {
+		
+		int marks=90;
+		
+		if(marks>80)
+		{
+			System.out.println("average");
+		}
+		else
+		{
+			System.out.println("good");
+		}
+	}
+
+}
